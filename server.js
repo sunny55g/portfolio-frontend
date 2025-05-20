b5ec3ee1f01207b66d3aae64d586818c2cb3831b
sunny55g <harshsuthar5656@gmail.com>
2025-05-20 17:49:04 +0530

    Initial commit of frontend

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
+require("dotenv").config();
 
+const cors = require('cors');
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
@@ -8,12 +10,14 @@ const app = express();
 const port = process.env.PORT || 3010;
 
 // Middleware
+app.use(cors());
+app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static('./'));  // Serve static files from the current directory
 
 // MongoDB Connection
-mongoose.connect('mongodb://localhost:27017/students', {
+mongoose.connect('mongodb+srv://harshsuthar5656:sunny_55g@newapi.rxlryp4.mongodb.net/contactFormDB?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
@@ -54,8 +58,31 @@ app.post('/post', async (req, res) => {
     res.status(500).json({ message: 'Failed to send message.' });
   }
 });
+app.post("/api/contact", async (req, res) => {
+  const { name, email, message } = req.body;
+  console.log("Received:", req.body);
+   try {
+    await Contact.create({ name, email, message });
+    res.status(201).json({ success: true });
+  } catch (err) {
+    console.error("Error saving contact:", err);
+    res.status(500).json({ success: false, error: "Database error" });
+  }
+});
 
 // Start the server
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+
+
+
+// mongoose.connect(process.env.MONGO_URI, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// })
+// .then(() => console.log("✅ MongoDB Connected"))
+// .catch(err => console.error("❌ MongoDB Error:", err));
+
+
+
